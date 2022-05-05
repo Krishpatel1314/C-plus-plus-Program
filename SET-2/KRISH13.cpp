35f4cbb575da09cedd5e587787e0463489aa3e57
Krish Patel 1314 <72722959+Krishpatel1314@users.noreply.github.com>
2022-05-05 11:31:48 +0530

    Add files via upload

---
+++ SET-2/KRISH13.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=6;
+    double d=13.5;
+    int &r=a;
+    double &s=d;
+    cout<<"Value of A and D:"<<a<<" & "<<d<<endl;
+    cout<<"Reference of A and D:"<<r<<" & "<<s;
+    return 0;
+}
