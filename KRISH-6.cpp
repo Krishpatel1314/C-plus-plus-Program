81423d9fd0dcfc904fa538b6bb2c0e01332bfb80
Krishpatel1314 <krishptel13@gmail.com>
2022-06-18 23:54:30 +0530

    ma

---
+++ KRISH-6.cpp
@@ -0,0 +1,131 @@
+#include<iostream>
+using namespace std;
+
+class date
+{
+    int day,month,year,leap;
+    public:
+    date()
+    {
+        day=0;
+        month=0;
+        year=0;
+    }
+    void get()
+    {
+        cout<<"Enter a day:";
+        cin>>day;
+        cout<<"Enter a month:";
+        cin>>month;
+        cout<<"Enter a year:";
+        cin>>year; 
+        int x=28;
+		if(year%4==0)
+		    x=29;
+        if(day<1)
+        {
+            cout<<"Error! Date does not exist!"<<endl;
+            get();
+        }
+        if(month<0 || month>13)
+        {
+            cout<<"Error! Date does not exist!"<<endl;
+            get();
+        }
+        if(month==1||month==3||month==5||month==7||month==8||month==10||month==12)
+		{
+			if(day>31)
+			{
+			   	cout<<"Day is more then 31."<<endl;
+                get();
+			}
+        }
+		else if(month==4||month==6||month==9||month==11)
+		{
+			if(day>30)
+			{
+			   	cout<<"Day is more then 30."<<endl;
+                get();
+			}
+		}
+		else if(month==2)
+		{
+			if(day>x)
+			{
+				cout<<"Day is more then "<<x<<"."<<endl;
+                get();
+			}
+		}
+		else
+		{	
+			cout<<"==========INVALID INPUT=============";
+		}
+    }
+    void display()
+    {
+        cout<<"The date is:"<<day<<"/"<<month<<"/"<<year<<endl;
+    }
+    void operator <(date j)
+    {
+        if (year<j.year)
+        {
+            cout<<"Date one is Greater"<<endl;
+        }
+        else if (year==j.year)
+        {
+            if (month<j.month)
+            cout<<"Date one is Greater"<<endl;
+        }
+        else if (month==j.month)
+        {
+            if(day>j.day)
+            cout<<"Date one is Greater"<<endl;
+        }    
+    }
+    void operator >(date j)
+    {
+        if (year>j.year)
+        {
+            cout<<"Date one is Smaller"<<endl;
+        }
+        else if (year==j.year)
+        {
+            if (month>j.month)
+            cout<<"Date one is Smaller"<<endl;
+        }
+        else if (month==j.month)
+        {
+            if(day>j.day)
+            cout<<"Date one is Smaller"<<endl;
+        }    
+    }
+    void operator ==(date j)
+    {
+        if (day==j.day && month==j.month && year==j.year)
+        cout<<"Both dates are equal"<<endl;
+    }
+    void operator !=(date j)
+    {
+        if (day!=j.day || month!=j.month || year!=j.year)
+        cout<<"Both dates are not equal"<<endl;
+    }
+	~date()
+    {
+        day=0;
+        month=0;
+        year=0;
+    }	
+};
+int main()
+{
+	date d1,d2;
+	d1.get();
+    d2.get();
+    d1.display();
+    d2.display();
+	d1<d2;
+    d1>d2;
+    d1==d2;
+    d1!=d2;
+	return 0;
+}
---
+++ KRISH-8-1.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+using namespace std;
+
+class complex
+{
+    float real, image;
+    public:
+    complex()
+    {
+        real=0;
+        image=0;
+    }
+    void get()
+    {
+        cout<<"Enter real number:";
+        cin>>real;
+        cout<<"Enter image number:";
+        cin>>image;
+    }
+    void display()
+    {
+        cout<<real<<"+"<<image<<"i"<<endl;
+    }
+    complex operator + (complex j)
+    {
+        complex t;
+        t.real=j.real+real;
+        t.image=j.image+image;
+        return t;
+    }
+    complex operator - (complex j)
+    {
+        complex t;
+        t.real=real-j.real;
+        t.image=image-j.image;
+        return t;
+    }
+    complex operator * (complex j)
+    {
+        complex t;
+        t.real=(real*j.real)-(image*j.image);
+        t.image=(image*j.real)+(real*j.image);
+        return t;
+    }
+    complex operator / (complex j)
+    {
+        complex t;
+        t.real=(real*j.real+image*j.image)/(j.real*j.real+j.image*j.image);
+        t.image=(j.real*j.real-j.image*j.image)/(j.real*j.real+j.image*j.image);
+        return t;
+    }
+    ~complex()
+    {
+        real=0;
+        image=0;
+    }
+};
+int main()
+{
+    complex c1,c2,c3;
+    c1.get();
+    c2.get();
+    cout<<"=================ADDITION===================="<<endl;
+    c3=c1+c2;
+    c3.display();
+    cout<<"================SUBTRACTION=================="<<endl;
+    c3=c1-c2;
+    c3.display();
+    cout<<"===============MULTIPLICATION================"<<endl;
+    c3=c1*c2;
+    c3.display();
+    cout<<"==================DIVISION==================="<<endl;
+    c3=c1/c2;
+    c3.display();
+    return 0;
+}
