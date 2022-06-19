6fa451e1dd26e905710aba74aa75393155188d53
Krish Patel 1314 <72722959+Krishpatel1314@users.noreply.github.com>
2022-06-19 16:37:20 +0530

    Add files via upload

---
+++ SET-2/KRISH11.cpp
@@ -0,0 +1,11 @@
+/*W.A.P. to check the effect of dynamic initialization.*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    float r;
+    cout<<"Enter radius:";
+    cin>>r;
+    cout<<"Area of circle="<<3.14*r*r;
+    return 0;
+}
