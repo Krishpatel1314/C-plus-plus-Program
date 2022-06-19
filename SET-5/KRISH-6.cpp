c480c5b8bac4a0c8b0123a1c15a0a2f8d1dc722c
Krish Patel 1314 <72722959+Krishpatel1314@users.noreply.github.com>
2022-06-19 18:48:27 +0530

    Add files via upload

---
+++ SET-5/KRISH-6.cpp
@@ -0,0 +1,128 @@
+/*Create class matrix with following data members
+int array[size][size] (size can vary), Use constructors and destructors properly and for performing various arithmetic operations write member functions for each of the following
+a. Addition function to add two matrices
+b. Subtraction function to add two matrices
+c. Multiplication function to add two matrices
+d. Division function to add two matrices
+e. Function to get data and to print data*/
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+class matrix
+{
+    float m[5][5];
+    int r,c;
+    public:
+    matrix(int r,int c)
+    {
+	this->r=r;
+        this->c=c;
+        for(int i=0;i<r;i++)
+    	{
+            for(int j=0;j<c;j++)
+	    m[i][j]=0;
+	}
+    }
+    void get()
+    {
+        for (int i = 0; i < r; i++)
+        {
+            for (int j = 0; j < c; j++)
+            {
+                cin>>m[i][j];
+            }  
+        }      
+    } 
+    void display()
+    {
+        for (int i = 0; i < r; i++)
+        {
+            for (int j = 0; j < c; j++)
+            {
+                cout<<setw(7)<<m[i][j];
+            }  
+            cout<<endl;
+        }  
+    }  
+    void add(matrix d,matrix e)
+    {
+        for (int i = 0; i < r; i++)
+        {
+            for (int j = 0; j < c; j++)
+            {
+                m[i][j]=d.m[i][j]+e.m[i][j];
+            }  
+        }
+        display();
+    }
+    void sub(matrix d,matrix e)
+    {
+        for (int i = 0; i < r; i++)
+        {
+            for (int j = 0; j < c; j++)
+            {
+                m[i][j]=d.m[i][j]-e.m[i][j];
+            }  
+        }
+        display();
+    }
+     void multi(matrix d,matrix e)
+    {
+        for (int i = 0; i < r; i++)
+        {
+            for (int j = 0; j < c; j++)
+            {
+                for (int k = 0; k < r; k++)
+                {
+                    m[i][j]=d.m[i][j]*e.m[i][j];
+                }
+                
+            }  
+        }
+        display();
+    }
+    void div(matrix d,matrix e)
+    {
+        for (int i = 0; i < r; i++)
+        {
+            for (int j = 0; j < c; j++)
+            {
+                m[i][j]=d.m[i][j]/e.m[i][j];
+            }  
+        }
+        display();
+    }
+    ~matrix()
+    {
+		r=0;
+        c=0;
+        for(int i=0;i<r;i++)
+    	{
+            for(int j=0;j<c;j++)
+			m[i][j]=0;
+		}
+    }
+};
+int main()
+{
+    int x,y;
+    cout<<"Enter no. of rows:";
+    cin>>x;
+    cout<<"Enter no. of columns:";
+    cin>>y;
+    matrix a(x,y),b(x,y),c(x,y);
+    cout<<"Enter the first matrix:"<<endl;
+    a.get();
+    cout<<"Enter the second matrix:"<<endl;
+    b.get();
+    cout<<"Addition of two matrix"<<endl;
+    c.add(a,b);
+    cout<<"Subtraction of two matrix"<<endl;
+    c.sub(a,b);
+    cout<<"Multiplication of two matrix"<<endl;
+    c.multi(a,b);
+    cout<<"Division of two matrix"<<endl;
+    c.div(a,b);
+    return 0;
+}
