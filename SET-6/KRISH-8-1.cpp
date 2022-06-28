49d703675ab8f96abe5353e5650320ba066772b1
Krishpatel1314 <krishptel13@gmail.com>
2022-06-29 00:46:08 +0530

    Operator Ouverloading

--- SET-6/KRISH-8-1.cpp
+++ SET-6/KRISH-8-1.cpp
@@ -22,28 +22,28 @@ class complex
     {
         cout<<real<<"+"<<image<<"i"<<endl;
     }
-    complex operator + (complex j)
+    complex operator +=(complex j)
     {
         complex t;
         t.real=j.real+real;
         t.image=j.image+image;
         return t;
     }
-    complex operator - (complex j)
+    complex operator -=(complex j)
     {
         complex t;
         t.real=real-j.real;
         t.image=image-j.image;
         return t;
     }
-    complex operator * (complex j)
+    complex operator *=(complex j)
     {
         complex t;
         t.real=(real*j.real)-(image*j.image);
         t.image=(image*j.real)+(real*j.image);
         return t;
     }
-    complex operator / (complex j)
+    complex operator /=(complex j)
     {
         complex t;
         t.real=(real*j.real+image*j.image)/(j.real*j.real+j.image*j.image);
@@ -62,16 +62,16 @@ int main()
     c1.get();
     c2.get();
     cout<<"=================ADDITION===================="<<endl;
-    c3=c1+c2;
+    c3=c1+=c2;
     c3.display();
     cout<<"================SUBTRACTION=================="<<endl;
-    c3=c1-c2;
+    c3=c1-=c2;
     c3.display();
     cout<<"===============MULTIPLICATION================"<<endl;
-    c3=c1*c2;
+    c3=c1*=c2;
     c3.display();
     cout<<"==================DIVISION==================="<<endl;
-    c3=c1/c2;
+    c3=c1/=c2;
     c3.display();
     return 0;
 }
