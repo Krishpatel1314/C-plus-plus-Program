ca401d79a68bc170c812336a5a4deba8156429d0
Krishpatel1314 <krishptel13@gmail.com>
2022-07-19 01:58:40 +0530

    Module

---
+++ Module/6-18.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+class bird{
+    char name;
+    int weight;
+    public:
+    bird()
+    {
+        cout<<"NAme:-";
+        cin>>name;
+        cout<<"Weight:-";
+        cin>>weight;
+    }
+    bird(char a,int x)
+    {
+      name=a;
+      weight=x;
+    }
+    void display()
+    {
+        cout<<name<<weight;
+    }
+    void operator >=(bird v)
+    {
+        if(v.weight>weight)
+        {
+            cout<<"False";
+        }
+    }
+};
+int main()
+{
+    bird b1,b2;
+    b1.display();
+    b2.display();
+    b1 >= b2;
+}
